24a72d61be20d782c7f34718003119f71b93fa61
Jorge Iván <jimartinezv@uqvirtual.edu.co>
2022-03-27 22:14:45 -0500

    Se agregan entidad codigo descuento

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/CodigoDescuento.java
@@ -0,0 +1,50 @@
+package co.edu.uniquindio.unitravel.entidades;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class CodigoDescuento implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private String codigo;
+
+    private LocalDateTime fechaCreacion;
+
+    private LocalDateTime fechaVencimiento;
+
+    private double descuento;
+
+    private String descripcion;
+
+    private Integer cantidadDisponible;
+
+    private Boolean activo;
+
+    @OneToMany(mappedBy = "codigoDescuento")
+    private List<Reserva> reservas;
+
+    public CodigoDescuento(String codigo, LocalDateTime fechaCreacion, LocalDateTime fechaVencimiento, double descuento, String descripcion, Integer cantidadDisponible, Boolean activo) {
+        this.codigo = codigo;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaVencimiento = fechaVencimiento;
+        this.descuento = descuento;
+        this.descripcion = descripcion;
+        this.cantidadDisponible = cantidadDisponible;
+        this.activo = activo;
+    }
+}
--- persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/ReservaHabitacion.java
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/ReservaHabitacion.java
@@ -29,4 +29,7 @@ public class ReservaHabitacion implements Serializable {
 
     @ManyToOne
     private Habitacion habitacion;
+
+    @ManyToOne
+    private CodigoDescuento codigoDescuento;
 }
